7115223a422da50e6f9926463c7787fd613169a9
sn-fullstackdev <sn.fullstackdev@gmail.com>
2018-06-28 07:10:22 +0800

    Navbar modified

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <title>Shiar Noralifkhan</title>
     </head>
     <body>
-        <nav class="navbar navbar-expand-md bg-dark">
+        <nav class="navbar fixed-top navbar-expand-md bg-dark">
             <div class="container">
                 <a class="navbar-brand" href="#">Shiar Noralifkhan</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
